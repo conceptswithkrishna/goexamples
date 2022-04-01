9229a5171507611165646f1e3165c2ca115b1704
Krishna Iyer Easwaran <krishna@thethingsindustries.com>
2022-04-01 17:21:43 +0200

    Update code

--- 1_http/main.go
+++ 1_http/main.go
@@ -3,25 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/KrishnaIyer/goexamples/1_http/pkg/server"
 	"github.com/gorilla/mux"
 )
 
 func main() {
-	address := "localhost:8080"
+	address := ":8080"
 	r := mux.NewRouter()
 	srv := server.New()
-
 	r.HandleFunc("/", srv.HandleIndex)
-	r.HandleFunc("/users/create", srv.HandleCreateUser)
-	r.HandleFunc("/users/{name}", srv.HandleUser)
-
+	r.HandleFunc("/users/create", srv.HandleCreateUsers)
+	r.HandleFunc("/users/{name}", srv.HandleUsers)
 	s := &http.Server{
-		Addr:    address,
-		Handler: r,
+		Addr:           address,
+		Handler:        r,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
-
-	log.Printf("Listening on port: %v", address)
+	log.Printf("Start server: %v", address)
 	log.Fatal(s.ListenAndServe())
 }
--- 1_http/pkg/server/server.go
+++ 1_http/pkg/server/server.go
@@ -13,80 +13,86 @@ var indexPage = `
 <!DOCTYPE html>
 <html>
 	<body>
-		<h1 style="text-align:center;"> User Database </h1>
-		<p style="text-align:center;"> 
-		Welcome to the user database.
-		</p>
+		<h1 style="text-align:center;" > User Database </h1>
+		<p style="text-align:center;" > Welcome to the user database. </p>
 	</body>
 </html>
 `
 
+// user represents the JSON value that's sent as a response to a request.
 type user struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
-	Age   uint8  `json:"age"`
+	Age   int    `json:"age"`
 }
 
+// userinfo is the information that is stored per user.
 type userinfo struct {
 	email string
-	age   uint8
+	age   int
 }
 
 // Server is an HTTP server.
 type Server struct {
-	users map[string]userinfo
+	users map[string]userinfo // key -> username
 }
 
-// New returns a new server.
+// New is a new server.
 func New() *Server {
 	return &Server{
 		users: make(map[string]userinfo),
 	}
 }
 
-// HandleIndex handles the index (root) route.
+// HandleIndex handles the index path "/".
 func (s *Server) HandleIndex(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/html") // Encoding standards
-	w.Write([]byte(indexPage))
+	w.Header().Add("content-type", "text/html")
 	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(indexPage))
 }
 
-// HandleCreateUser handles the `/user/create` POST/PUT route.
-func (s *Server) HandleCreateUser(w http.ResponseWriter, r *http.Request) {
+// HandleCreateUsers handles the path "/users/create".
+// Create -> Post/Put.
+func (s *Server) HandleCreateUsers(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost, http.MethodPut:
-		contentType := r.Header.Get("Content-Type")
-		if contentType != "application/json" {
-			w.WriteHeader(http.StatusUnsupportedMediaType) // HTTP 415
+		// Check that the input is JSON.
+		if contentType := r.Header.Get("Content-Type"); contentType != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
 			return
 		}
 
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Could not read request body: %v", err)
+			w.WriteHeader(http.StatusInternalServerError) // HTTP 500
 			return
 		}
 		defer r.Body.Close()
 
-		var user user
-		err = json.Unmarshal(body, &user)
+		// Unmarshal the body.
+		var u user
+		err = json.Unmarshal(body, &u)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			log.Printf("Could not unmarshal request body: %v", err)
+			w.WriteHeader(http.StatusBadRequest) // HTTP 400
 			return
 		}
-		log.Printf("Create user: %v", user.Name)
-		s.users[user.Name] = userinfo{
-			email: user.Email,
-			age:   user.Age,
+
+		log.Printf("Create User: %v", u.Name)
+		s.users[u.Name] = userinfo{
+			email: u.Email,
+			age:   u.Age,
 		}
-		w.WriteHeader(http.StatusOK)
+
 	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed) // HTTP 415
 	}
 }
 
-// HandleUser handles the HTTP Get method on `/users/` path
-func (s *Server) HandleUser(w http.ResponseWriter, r *http.Request) {
+// HandleUsers handles the `/users/{name}` request.
+func (s *Server) HandleUsers(w http.ResponseWriter, r *http.Request) {
+	// Fetch the name from the params. Common for all methods of this route.
 	params := mux.Vars(r)
 	name := params["name"]
 	u, ok := s.users[name]
@@ -94,6 +100,7 @@ func (s *Server) HandleUser(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+
 	switch r.Method {
 	case http.MethodGet:
 		ret := user{
@@ -103,51 +110,51 @@ func (s *Server) HandleUser(w http.ResponseWriter, r *http.Request) {
 		}
 		msg, err := json.Marshal(ret)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Could not marshal request: %v", err)
+			w.WriteHeader(http.StatusInternalServerError) // HTTP 500
 			return
 		}
-		log.Printf("Found user: %v", name)
+		log.Printf("Get user: %s", name)
 		w.Header().Add("Content-Type", "application/json")
 		w.Write(msg)
 	case http.MethodPatch:
-		contentType := r.Header.Get("Content-Type")
-		if contentType != "application/json" {
-			w.WriteHeader(http.StatusUnsupportedMediaType) // HTTP 415
+		// Partial updates.
+		// Check that the input is JSON.
+		if contentType := r.Header.Get("Content-Type"); contentType != "application/json" {
+			w.WriteHeader(http.StatusUnsupportedMediaType)
 			return
 		}
-
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Could not read request body: %v", err)
+			w.WriteHeader(http.StatusInternalServerError) // HTTP 500
 			return
 		}
 		defer r.Body.Close()
 
-		var user user
-		err = json.Unmarshal(body, &user)
+		// Unmarshal the body.
+		var u user
+		err = json.Unmarshal(body, &u)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
+			log.Printf("Could not unmarshal request body: %v", err)
+			w.WriteHeader(http.StatusBadRequest) // HTTP 400
 			return
 		}
-		log.Printf("Update user: %v", name)
+
+		log.Printf("Update user: %s", name)
 
 		userinfo := s.users[name]
-		if user.Age != 0 {
-			userinfo.age = user.Age
+		if u.Age != 0 {
+			userinfo.age = u.Age
 		}
-		if user.Email != "" {
-			userinfo.email = user.Email
+		if u.Email != "" {
+			userinfo.email = u.Email
 		}
 		s.users[name] = userinfo
 	case http.MethodDelete:
-		_, ok := s.users[name]
-		if !ok {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-		log.Printf("Delete user: %v", name)
+		log.Printf("Delete user: %s", name)
 		delete(s.users, name)
 	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.WriteHeader(http.StatusMethodNotAllowed) // HTTP 415
 	}
 }
